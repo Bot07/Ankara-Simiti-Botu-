82403d17a604532b76c29ba79c46252c80698f5a
domates06 <35399633+Bot07@users.noreply.github.com>
2018-07-27 17:18:33 +0300

    Update asbbot.js

--- asbbot.js
+++ asbbot.js
@@ -5,7 +5,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Bot${client.user.tag}adı ile giriş yaptı!`);
 });
-client.on("ready", () => {client.user.setGame('o!yardım | Eski Sürümü Yükledim Güncelleme Olmayacak | Sunucu Sayısı: ' + client.guilds.size , 'https://twitch.tv/GardropFuat') 
+client.on("ready", () => {client.user.setGame('Hatıra Kalması İçin | Güncelleme Olmayacak | Sunucu Sayısı: ' + client.guilds.size , 'https://twitch.tv/GardropFuat') 
 
 
 
